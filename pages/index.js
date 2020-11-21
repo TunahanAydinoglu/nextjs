b4a8a5762ca1402fdd2a7efb53d115dd33484788
Tunahan <tunahanaydinoglu@outlook.com>
2020-11-21 21:54:41 +0300

    Stroybook integration

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Button from "../components/button";
+
+function HomePage() {
+    return <div><h1>Welcome to Next.js!</h1>
+        <Button>Say my name</Button>
+    </div>
+  }
+  
+  export default HomePage
